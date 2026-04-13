1d275f684ed6e8de4a50bf57383f617cef162317
zan-luka <zanluka.nusdorfer@gm ail.com>
2026-04-13 13:07:38 +0200

    nov model za zaznavo

--- demo/components/pose-landmarker/constants.ts
+++ demo/components/pose-landmarker/constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_MODEL_PATH = "/models/pose_landmarker_full.task";
+export const DEFAULT_MODEL_PATH = "/models/pose_landmarker_heavy.task";
 export const MEDIAPIPE_WASM_BASE_URL =
 	"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.34/wasm";
 export const DEFAULT_CAPTURE_EVERY_NTH_FRAME = 1;
